ef9c9552a2f5bb7b9c6a08e27759bfc45161cfee
Baejiu <jiumaker@naver.com>
2025-02-09 17:09:38 +0900

    feat: 네트워크 에러 시 재시도 하도록 수정

--- src/network/http.js
+++ src/network/http.js
@@ -1,35 +1,64 @@
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
+
+const defaultRetryConfig = {
+  retries: 5,
+  initialDelayMs: 100,
+};
 export default class HttpClient {
-  constructor(baseURL, authErrorEventBus, getCsrfToken) {
+  constructor(
+    baseURL,
+    authErrorEventBus,
+    getCsrfToken,
+    config = defaultRetryConfig
+  ) {
     this.baseURL = baseURL;
     this.authErrorEventBus = authErrorEventBus;
     this.getCsrfToken = getCsrfToken;
+    this.client = axios.create({
+      baseURL: baseURL,
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    });
+    axiosRetry(this.client, {
+      retries: config.retries,
+      retryDelay: (retry) => {
+        const delay = Math.pow(2, retry) * config.initialDelayMs;
+        const jitter = delay * 0.1 * Math.random();
+        return delay + jitter;
+      },
+      retryCondition: (err) =>
+        axiosRetry.isNetworkOrIdempotentRequestError(err) ||
+        err.response.status === 429,
+    });
   }
   async fetch(url, options) {
-    const res = await fetch(`${this.baseURL}${url}`, {
-      ...options,
+    const { body, method, headers } = options;
+    const request = {
+      url,
+      method,
       headers: {
-        'Content-Type': 'application/json',
-        ...options.headers,
+        ...headers,
         '_csrf-token': this.getCsrfToken(),
       },
-      credentials: 'include',
-    });
-    let data;
+      data: body,
+    };
     try {
-      data = await res.json();
-    } catch (error) {
-      console.error(error);
-    }
-    if (res.status > 299 || res.status < 200) {
-      const message =
-        data && data.message ? data.message : 'Someting went wrong!';
-      const error = new Error(message);
-      if (res.status === 401) {
-        this.authErrorEventBus.notify(error);
-        return;
+      const res = await this.client(request);
+      return res.data;
+    } catch (err) {
+      if (err.res) {
+        const data = err.res.data;
+        const message =
+          data && data.message ? data.message : 'Someting went wrong!';
+
+        if (err.res.status === 401) {
+          this.authErrorEventBus.notify(err);
+          return;
+        }
+        throw new Error(message);
       }
-      throw error;
+      throw new Error('connection error');
     }
-    return data;
   }
 }
